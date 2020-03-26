b1c1fa3e0f6ab8183e9c54512ad5cf0e0639e682
Daniel Ontiveros <cardanont@gmail.com>
2020-03-25 23:58:30 -0600

    Client calss with client properties

---
+++ ClientManagement.Model/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClientManagement.Model
+{
+    public class Client
+    {
+        public int ClientId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Gender Gender { get; set; }
+        public string PhotoPath { get; set; }
+    }
+}
